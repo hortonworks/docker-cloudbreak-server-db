7bd466c553b9ccbd58d29d87dbdbce7c10d9aaf3
lalyos <lalyos@yahoo.com>
2015-05-05 19:54:12 +0200

    initial version

---
+++ create-db-tgz.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+start_db(){
+  cbd startdb
+  cbd migrate cbdb up
+  if cbd migrate cbdb status|grep "MyBatis Migrations SUCCESS" ; then
+      echo Migration: OK
+  else
+      echo Migration: ERROR
+      exit 1
+  fi
+}
+
+db_backup() {
+    local ver=$(cbd env export|grep DOCKER_TAG_CLOUDBREAK|sed "s/.*=//")
+
+    mkdir -p release
+    docker exec  cbreak_cbdb_1 tar cz -C /var/lib/postgresql/data . > release/cbdb-${ver}.tgz
+    docker exec  cbreak_pcdb_1 tar cz -C /var/lib/postgresql/data . > release/pcdb-${ver}.tgz
+}
+
+main() {
+    start_db
+    db_backup
+}
+
+[[ "$0" ==  "$BASH_SOURCE" ]] && main "$@"
