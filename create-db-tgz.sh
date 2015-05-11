79d88f30aebb95822c734a5b1a8e1da08bde66f2
lalyos <lalyos@yahoo.com>
2015-05-11 19:31:15 +0200

    Add clean and release

--- create-db-tgz.sh
+++ create-db-tgz.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-: ${VERSION:=0.5.32}
+DBNAME=cbdb
 
 start_db(){
+
+  declare ver=${1:? version required}
+  
   echo 'export PUBLIC_IP=1.1.1.1'>Profile
-  echo "export DOCKER_TAG_CLOUDBREAK=$VERSION" >> Profile
+  echo "export DOCKER_TAG_CLOUDBREAK=$ver" >> Profile
   cbd init
   #cbd pull
   
   cbd startdb
-  cbd migrate cbdb up
-  if cbd migrate cbdb status|grep "MyBatis Migrations SUCCESS" ; then
+  cbd migrate $DBNAME up
+  if cbd migrate $DBNAME status|grep "MyBatis Migrations SUCCESS" ; then
       echo Migration: OK
   else
       echo Migration: ERROR
@@ -19,18 +22,30 @@ start_db(){
 }
 
 db_backup() {
-    local ver=$(cbd env export|grep DOCKER_TAG_CLOUDBREAK|sed "s/.*=//")
+
+    declare ver=${1:? version required}
 
     # for gracefull shutdown: run another containe with --volumes from
-    # docker exec cbdb bash -c 'kill -INT $(head -1 /var/lib/postgresql/data/postmaster.pid)'
+    # docker exec $DBNAME bash -c 'kill -INT $(head -1 /var/lib/postgresql/data/postmaster.pid)'
     
     mkdir -p release
-    docker exec  cbreak_cbdb_1 tar cz -C /var/lib/postgresql/data . > release/cbdb-${ver}.tgz
+    docker exec  cbreak_$DBNAME_1 tar cz -C /var/lib/postgresql/data . > release/$DBNAME-${ver}.tgz
+}
+
+clean() {
+    rm -rf Profile *.yml release/
+}
+
+release() {
+    declare ver=${1:? version required}
+    gh-release create sequenceiq/docker-$DBNAME "${ver}"
 }
 
 main() {
-    start_db
-    db_backup
+    clean
+    start_db "$@"
+    db_backup "$@"
+    release "$@"
 }
 
 [[ "$0" ==  "$BASH_SOURCE" ]] && main "$@"
